96bcc7482c802accffd6fa91aa014a75536b5ce8
Boris HUISGEN <bhuisgen@hbis.fr>
2015-03-18 15:03:48 +0100

    Ignore linting on app/scripts/templates.js
    Signed-off-by: Boris HUISGEN <bhuisgen@hbis.fr>

--- Gruntfile.js
+++ Gruntfile.js
@@ -138,6 +138,7 @@ module.exports = function(grunt) {
             all: [
                 'Gruntfile.js',
                 '<%= config.app %>/scripts/{,*/}*.js',
+                '!<%= config.app %>/scripts/templates.js',
                 '!<%= config.app %>/scripts/vendor/*',
                 'test/spec/{,*/}*.js'
             ]
